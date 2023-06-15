7dbe7122c4fe837931228354731b0a87e5cbbd41
Sreerag <sreeragatk3@gmail.com>
2023-06-15 17:20:16 +0530

    Update startup

--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+// const helmet = require('helmet');
+// const cors = require('cors');
 const error = require('../middlewares/error');
 const product = require('../routes/products.router');
 const cart = require('../routes/cart.router');
@@ -12,7 +12,9 @@ const wishlist = require('../routes/wishlist.router');
 
 module.exports = function(app) {
 	//app.use(helmet());
-	app.use(cors());
+	//app.use(cors());
+	
+	
 	app.use(express.json({ limit: '2mb' }));
 	app.use(express.urlencoded({ limit: '2mb', extended: true }));
 	app.enable('trust proxy'); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
